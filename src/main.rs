81c9ff25d173c689164deaa704f964e34bea39cd
Dan Lock <codednil@gmail.com>
2023-05-21 21:29:06 +0100

    Added iced

--- src/main.rs
+++ src/main.rs
@@ -126,5 +126,12 @@ fn main() {
 
     // Add item (generates description, photo_description, image)
 
+
+
+    // Flow of the gameplay loop from DM perspective
+    // DM at any point has buttons to create factions, variants, animals, etc etc, to be used in future
+    // Create a zone, either brief description provided or let the AI generate it
+    // AI then generates all the rooms of the zone and joins them up, placing animals and npcs etc where needed, and placing loot and puzzles etc
+
     // Create game ui with journal to view all the data, and buttons to add new data
 }
