0a3366b09ea1fce2836a4688aa15303b42fa5be6
CodeNil <codenil@proton.me>
2023-05-23 12:38:38 +0100

    Use dioxus

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,37 @@
-mod apis;
-mod data;
-mod ui;
+use dioxus::prelude::*;
 
 fn main() {
-    // Apis to access and change the data
-    // Add NPC, species & subspecies (generates name, status, history, items)
-    // Add species, name (generates description, photo_description, image, status_towards_players, 1 subspecies)
-    // Add subspecies, name (generates description, photo_description, image)
+    dioxus_web::launch(app);
+}
 
-    // Add room (generates description, photo_description, image)
+fn app(cx: Scope) -> Element {
+    let column_width = 200.0;
 
-    // Add player character (Users customisation UI with buttons to generate things) (generates items)
-
-    // Add item (generates description, photo_description, image)
-
-    // Flow of the gameplay loop from DM perspective
-    // DM at any point has buttons to create factions, variants, animals, etc etc, to be used in future
-    // Create a zone, either brief description provided or let the AI generate it
-    // AI then generates all the rooms of the zone and joins them up, placing animals and npcs etc where needed, and placing loot and puzzles etc
-
-    // Create game ui with journal to view all the data, and buttons to add new data
-    ui::create();
+    cx.render(rsx! {
+        div {
+            display: "flex",
+            height: "calc(100vh - 16px)",
+            div {
+                class: "main_content",
+                flex: "1",
+                padding: "0",
+                background_color: "#C5E1A5",
+                border_radius: "10px",
+                "Main Content"
+            }
+            div {
+                class: "separator",
+                resize: "horizontal",
+                cursor: "col-resize",
+                width: "10px",
+            }
+            div {
+                class: "side_content",
+                width: column_width,
+                background_color: "#FFF59D",
+                border_radius: "10px",
+                "Side Content"
+            }
+        }
+    })
 }
